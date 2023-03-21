6eeeec91fa8c031693a20823a03bc3c904c92840
Lindsey <paklindsey@gmail.com>
2023-03-21 14:07:22 -0400

    navbar init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 3dPortfolio
+
+### starting dev env
+
+`npm run dev`
+
+### time stamps
+
+3.21 - 34:20
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
 function App() {
-  return <div className="App">3d project</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
